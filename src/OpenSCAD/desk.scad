f8487a34cacb4e2853fe2148f1786845f0a0931e
Timothy Schmidt <timschmidt@gmail.com>
2021-03-15 07:33:59 -0400

    Add cutter, fix up desk

--- src/OpenSCAD/desk.scad
+++ src/OpenSCAD/desk.scad
@@ -12,40 +12,48 @@ include <MCAD/materials.scad>
 
 frame_is_hollow = 0;
 
-color(Pine) {
-
 // Vertical 
-translateFrame([0,14,0]) zFrame(40);
-translateFrame([29,5,0]) zFrame(40);
-translateFrame([0,5,0]) zFrame(40);
-translateFrame([29,14,0]) zFrame(40);
+translateFrame([2,15,0]) zFrame(40);
+translateFrame([31,6,0]) zFrame(40);
+translateFrame([0,6,0]) zFrame(40);
+translateFrame([29,15,0]) zFrame(40);
 
 // Feet
-translateFrame([1,0,0]) yFrame(15);
-translateFrame([28,0,0]) yFrame(15);
+translateFrame([1,1,0]) yFrame(15);
+translateFrame([30,1,0]) yFrame(15);
 
-// Shelving
-translateFrame([0,13,1]) xFrame(30);
-translateFrame([0,6,1]) xFrame(30);
-translateFrame([0,5,2]) topShelf(30, 10, 1);
+// Bottom shelf
+translateFrame([1,14,1]) xFrame(30);
+translateFrame([1,7,1]) xFrame(30);
+translateFrame([1,5,2]) topShelf(30, 10, 0);
 
-translateFrame([0,13,10]) xFrame(30);
-translateFrame([0,6,10]) xFrame(30);
-translateFrame([0,5,11]) topShelf(30, 10, 1);
+// Middle shelf
+translateFrame([1,14,10]) xFrame(30);
+translateFrame([1,7,10]) xFrame(30);
+translateFrame([1,6,9]) yFrame(10);
+translateFrame([30,6,9]) yFrame(10);
+translateFrame([1,5,11]) topShelf(30, 10, 0);
 
-translateFrame([1,0,20]) yFrame(15);
-translateFrame([28,0,20]) yFrame(15);
-translateFrame([0,13,19]) xFrame(30);
-translateFrame([0,6,19]) xFrame(30);
-translateFrame([0,0,21]) topShelf(30, 15, 0);
+// Desk surface
+translateFrame([1,1,19]) yFrame(15);
+translateFrame([30,1,19]) yFrame(15);
+translateFrame([1,14,20]) xFrame(30);
+translateFrame([1,1,20]) xFrame(30);
+translateFrame([1,0,21]) topShelf(30, 15, 0);
 
-translateFrame([0,13,38]) xFrame(30);
-translateFrame([0,6,38]) xFrame(30);
-translateFrame([0,5,39]) topShelf(30, 10, 1);
+// Hanging shelf
+translateFrame([15,14,29]) xFrame(15);
+translateFrame([15,5,29]) xFrame(15);
+translateFrame([28,6,29]) xFrame(4);
+translateFrame([30,5,29]) xFrame(2);
+translateFrame([15,15,29]) zFrame(10);
+translateFrame([15,6,29]) zFrame(10);
+translateFrame([16,5,30]) topShelf(15,10,0);
 
-translateFrame([15,13,29]) xFrame(15);
-translateFrame([15,6,29]) xFrame(15);
-translateFrame([15,14,29]) zFrame(10);
-translateFrame([15,5,29]) zFrame(10);
-translateFrame([15,5,30]) topShelf(15,10,1);
-}
+// Top shelf
+translateFrame([1,6,37]) yFrame(10);
+translateFrame([30,6,37]) yFrame(10);
+translateFrame([1,5,39]) topShelf(30, 10, 0);
+translateFrame([1,14,38]) xFrame(30);
+translateFrame([1,7,38]) xFrame(30);
+
--- src/OpenSCAD/generate-printable-files.bat
+++ src/OpenSCAD/generate-printable-files.bat
@@ -14,121 +14,15 @@ REM experimental OpenJSCAD binary generation
 REM openjscad objects/hole\ pattern/motors.jscad -o motors.dxf
 REM openjscad objects/hole\ pattern/grids.jscad --spacing 25.4 -o grids-1inch.dxf
 
-REM standard size frames 2, 3, 4, 5, 10, 15, 20, 25, 30, 40, and 50 holes in length rendered in hollow metal tube.
-if exist ../../bin/stl/38.1mm/frame-2.stl (
-    rem frame-2.stl exists
-) else (
-    "C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-2.stl -D holes=2 frame.scad
+for %%i in (*) do (
+    if exist ../../bin/stl/38.1mm/%%i.stl (
+        rem exists
+    ) else (
+        "C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/%%i.stl -D holes=2 %%i
+    )
+	if exist ../../bin/png/38.1mm/%%i.png (
+        rem exists
+    ) else (
+        "C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/%%i.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=3 %%i
+    )
 )
-if exist ../../bin/png/38.1mm/frame-2.png (
-	rem frame-2.png exists
-) else (
-	"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-2.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=2 frame.scad
-)
-
-
-
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-3.stl -D holes=3 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-3.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=3 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-4.stl -D holes=4 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-4.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=4 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-5.stl -D holes=5 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-5.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=5 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-10.stl -D holes=10 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-10.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=10 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-15.stl -D holes=15 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-15.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=15 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-20.stl -D holes=20 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-20.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=20 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-25.stl -D holes=25 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-25.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=25 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-30.stl -D holes=30 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-30.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=30 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-40.stl -D holes=40 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-40.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=40 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/frame-50.stl -D holes=50 frame.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/frame-50.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D holes=50 frame.scad
-
-REM bolts
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/bolt-1.stl -D length=50 bolts.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/bolt-1.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D length=50 bolts.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/bolt-2.stl -D length=90 bolts.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/bolt-2.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D length=90 bolts.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/bolt-3.stl -D length=130 bolts.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/bolt-3.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 -D length=130 bolts.scad
-
-REM nuts
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/nut.stl -D length=1 nuts.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/nut.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 nuts.scad
-
-REM linear bearings
-if exist ../../bin/stl/38.1mm/linear-bearing.stl (
-	REM linear-bearing.stl exists
-) else (
-	"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/linear-bearing.stl -D length=1 linear-bearing.scad
-)
-if exist ../../bin/png/38.1mm/linear-bearing.png (
-	REM linear-bearing.png exists
-) else (
-	"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/linear-bearing.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 linear-bearing.scad
-)
-REM washers
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/washers.stl -D length=1 washers.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/washers.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 washers.scad
-
-REM barrels
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/barrels.stl -D length=1 barrels.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/barrels.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 barrels.scad
-
-REM electrical connectors
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/electrical-connectors.stl -D length=1 electrical-connectors.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/electrical-connectors.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 electrical-connectors.scad
-
-REM bicycle bottom bracket adapter
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/adapter-bicycle-bottom-bracket.stl -D length=1 adapter-bicycle-bottom-bracket.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/adapter-bicycle-bottom-bracket.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 adapter-bicycle-bottom-bracket.scad
-
-REM wrench
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/wrench.stl -D length=1 wrench.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/wrench.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 wrench.scad
-
-REM plate-5x1
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/plate-5x1.stl -D length=1 plate-5x1.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/plate-5x1.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 plate-5x1.scad
-
-REM plate-5x10
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/plate-5x10.stl -D length=1 plate-5x10.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/plate-5x10.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 plate-5x10.scad
-
-REM plate-15x10
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/plate-15x10.stl -D length=1 plate-15x10.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/plate-15x10.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 plate-15x10.scad
-
-
-REM techniques
-
-REM tri joint
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/tri-joint.stl tri-joint.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/tri-joint.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 tri-joint.scad
-
-REM shelf joint - ends
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/shelf-joint-ends.stl shelf-joint.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/shelf-joint-ends.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 shelf-joint.scad
-
-REM shelf joint - width
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/shelf-joint-width.stl shelf-joint-2.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/shelf-joint-width.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 shelf-joint-2.scad
-
-REM barrel-cage
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/barrel-cage.stl -D length=1 barrel-cage.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/barrel-cage.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 barrel-cage.scad
-
-REM projects
-
-REM bookshelf
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/bookshelf.stl bookshelf.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/bookshelf.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 bookshelf.scad
-
-REM work cube
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/stl/38.1mm/work-cube.stl work-cube.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o ../../bin/png/38.1mm/work-cube.png --autocenter --viewall --view=scales --colorscheme=Metallic --imgsize=1024,1024 work-cube.scad
